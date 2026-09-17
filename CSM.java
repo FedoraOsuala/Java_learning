6cf0234074a715766f281c964088c6b563bef03a
FedoraOsuala <joyosuala17@gmail.com>
2026-09-17 14:40:28 +0100

    java learning

---
+++ CSM.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CSM {
+public static void main (String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter first integer:"); // prompt
+int number1 = input.nextInt(); // read first number from user
+System.out.print("Enter second integer:"); // prompt
+int number2 = input.nextInt(); // read second number from user
+int sum = number1 + number2; // add numbers, then store total in sum
+System.out.printf("sum is %d%n", sum); // display sum
+}
+}
